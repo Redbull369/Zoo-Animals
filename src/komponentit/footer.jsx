c25f8853faec812c0081c08bf185761a7e10aed5
Matilainen Olli <s2401194@edu.bc.fi>
2025-01-08 06:36:05 +0200

    Navigointipalkki ja Footer

---
+++ src/komponentit/footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer style={{
+            textAlign: 'center',
+            padding: '20px',
+            backgroundColor: '#282c34',
+            color: 'white',
+            position: 'fixed',
+            bottom: 0,
+            width: '100%'
+        }}>
+            <p>&copy; 2025 Olli Matilainen. Zoo / Animals React-projekti .</p>
+        </footer>
+    );
+};
+
+export default Footer;
